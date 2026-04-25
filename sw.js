0f2561abd414545f6af365b077e4fefce476ff59
danibanon <44100398+danibanon@users.noreply.github.com>
2026-04-25 21:06:40 +0200

    Add PWA install support

---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME='chord-detective-v1';
+const APP_SHELL=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(APP_SHELL))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+    )).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+  const isShellAsset=APP_SHELL.some(asset=>{
+    if(asset==='./')return false;
+    return url.pathname.endsWith(asset.replace('./','/'));
+  });
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req)
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy));
+          return res;
+        })
+        .catch(async()=>(
+          await caches.match(req)
+          || await caches.match('./index.html')
+          || await caches.match('./')
+        ))
+    );
+    return;
+  }
+
+  if(isShellAsset){
+    event.respondWith(
+      caches.match(req).then(async cached=>{
+        const network=fetch(req).then(res=>{
+          caches.open(CACHE_NAME).then(cache=>cache.put(req,res.clone()));
+          return res;
+        }).catch(()=>cached);
+        return cached||network;
+      })
+    );
+  }
+});
